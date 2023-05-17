ac8a5f2ac7f57b71193ec50b51aa8f8508832ea7
Ishan Bhardwaj <bhardwajishan8@gmail.com>
2023-05-17 11:37:54 +0100

    First controller + dependencies

--- src/main/java/io/laymen/springnotes/rest/HelloRestController.java
+++ src/main/java/io/laymen/springnotes/rest/HelloRestController.java
@@ -1,16 +1,19 @@
 package io.laymen.springnotes.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
-    // expose "/" that returns "Hello World"
+    @Value("${user.name}")  // Fetches the value of user.name property from application.properties file (by default)
+    private String name;
 
+    // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return "Hello " + name;
     }
 
 }
